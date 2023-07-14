7acc350336ec27ed3fc31491d391f3662113cb73
Meera Murthy <colorcodedcrayons@gmail.com>
2023-07-14 12:09:23 +0530

    Shopping cart add and delete functions

---
+++ src/store/cart-slice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        itemsList: [],
+        totalQuantity: 0,
+        showCart: false
+    },
+    reducers: {
+        addToCart (state, action) {
+            const newItem = action.payload;
+            console.log(state.itemsList)
+            const existItem = state.itemsList.find((item) => item.id === newItem.id)
+            if (existItem) {
+                console.log("Item exists")
+                existItem.quantity ++;
+                existItem.totalPrice += newItem.price;
+            }
+            else {
+                state.itemsList.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1, 
+                    totalPrice: newItem.price,
+                    name: newItem.name
+                })
+                
+            }
+            state.totalQuantity++;
+        },
+        removeFromCart (state, action) {
+            const id = action.payload;
+            const existItem = state.itemsList.find((item) => item.id === id)
+            if (existItem) {
+                if(existItem.quantity === 1) {
+                    state.itemsList = state.itemsList.filter(item => item.id !== id)
+                }
+                else {
+                    existItem.quantity --;
+                    existItem.totalPrice -= existItem.price;
+                }
+                state.totalQuantity--;
+            }
+        },
+        setShowCart (state) {
+                state.showCart = !state.showCart;
+        }
+    }
+})
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
